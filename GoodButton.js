cc02bd3675b99eaccbf928a3dbd41d1b020984a5
Ryoichi Gokan <nargok@gmail.com>
2018-04-15 11:02:40 +0900

    callback地獄

--- GoodButton.js
+++ GoodButton.js
@@ -1,15 +1,22 @@
+// 処理の順版
+// 1. いいねボタンを押す
+// 2. サーバへクリックイベントを送信する
+// 3. サーバからいいねボタンを押した回数を取得する
+// 4. いいねボタンを押した回数をconsoleへ出力する
+
 class GoodButton {
   constructor() {
     this.clickCount = 0;
   }
 
-  coundUpClickCount() {
+  countUpClickCount() {
     const xhr = new XMLHttpRequest();
 
     // callback 非同期処理終了後に実行してほしい関数を指定する
     xhr.onload = () => {
       const res = xhr.responseText;
       this.clickCount = countFromResponse(res);
+      callback();
     };
 
     // 非同期処理で実行する
@@ -18,9 +25,10 @@ class GoodButton {
   }
 }
 
+// 回数を表示する処理:　回数をサーバから受け取った後に表示するため非同期処理にする
 const onClickButton = (goodButton) => {
-  // ボタンを押した回数を1つ増やす
-  goodButton.clickCount += 1;
-  // 回数を表示
-  console.log(goodButton.clickCount);
+  goodButton.countUpClickCount(() => {
+    // 回数を表示
+    console.log(goodButton.clickCount);
+  });
 }
