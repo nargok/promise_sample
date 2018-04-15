1ea4df587ac1df26ab2c140464c838a9233b15de
Ryoichi Gokan <nargok@gmail.com>
2018-04-15 10:34:03 +0900

    callbackを使った非同期処理

--- GoodButton.js
+++ GoodButton.js
@@ -2,6 +2,20 @@ class GoodButton {
   constructor() {
     this.clickCount = 0;
   }
+
+  coundUpClickCount() {
+    const xhr = new XMLHttpRequest();
+
+    // callback 非同期処理終了後に実行してほしい関数を指定する
+    xhr.onload = () => {
+      const res = xhr.responseText;
+      this.clickCount = countFromResponse(res);
+    };
+
+    // 非同期処理で実行する
+    xhr.open('POST', 'https://(URL)', true) // 3番目の引数が非同期処理をonにする
+    xhr.send();
+  }
 }
 
 const onClickButton = (goodButton) => {
